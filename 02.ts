427c6bb0dc2841827c02feb0c746db3ee751e1ae
dongwook98 <ehddnr8563@gmail.com>
2023-09-21 14:28:16 +0900

    never타입, 느낌표

--- 02.ts
+++ 02.ts
@@ -20,3 +20,37 @@ const arr6 = [1, 2, 'hello']; // 타입스크립트 추론 잘못하고있음.
 // | 파이프 사용함 :(string | number)[]
 
 const obj2 = { age: 26, name: '강동욱' };
+
+// never 타입
+
+// ❌ 빈배열에 타이핑을 안하면 never 타입 추론
+try {
+  const array = [];
+  array.push('hello');
+} catch (error) {
+  error;
+}
+
+// 👍 빈배열에는 타이핑을 반드시 해주자.
+try {
+  const array2: string[] = [];
+  array2.push('hello');
+} catch (error) {
+  error;
+}
+
+// | 또는
+const head = document.querySelector('#head');
+
+// 느낌표(!) : null이나 undefined가 아니라고 확신할때만 사용! 하지만 사용하는거는 비추천한다.
+
+const head2 = document.querySelector('#head')!;
+// ❌ 누가 ID가 head인 태그를 header로 수정하면 에러가 남
+head.innerHTML = 'hello world';
+console.log(head);
+
+// 👍 If로 감싸주면 head가 null이면 블록이 실행안되므로 에러가 안남!
+if (head) {
+  head.innerHTML = 'hello world';
+  console.log(head);
+}
