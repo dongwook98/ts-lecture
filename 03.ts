9604245057bb7f7e7cc8beaa4bb8f3bea4e5f1ff
dongwook98 <ehddnr8563@gmail.com>
2023-11-14 20:29:18 +0900

    11/14 복습

--- 03.ts
+++ 03.ts
@@ -1,28 +1,27 @@
 // 원시 래퍼 타입
 
 const hello_3: string = 'hello';
-const hell_3: String = 'hell'; // 타입에 대문자 ❌
+const hell_3: String = 'hell'; // 타입에 대문자 X
 
 function c_3(a1: string, b2: string) {}
-c_3(hello_3, hell_3); // 🚨 String 이라 에러
+c_3(hello_3, hell_3); // String 이라 에러
 
 // 템플릿 리터럴 타입
 type World_3 = 'world' | 'hell';
-const aaa: World_3 = 'world';
+const aaa_3: World_3 = 'world';
 
-const bbb = `hello ${aaa}`;
+const bbb_3 = `hello ${aaa_3}`;
 
-type Greeting = `hello ${World_3}`;
-const ccc: Greeting = 'hello world'; // 자동완성도 지원해줌
+type Greeting_3 = `hello ${World_3}`;
+const ccc_3: Greeting_3 = 'hello hell'; // 자동완성도 지원해줌
 
 // rest
-function rest(a: number, ...args: string[]) {
+function rest_3(a: number, ...args: string[]) {
   console.log(a, args); // 1, ["2", "3"]
 }
-rest(1, '2', '3');
+rest_3(1, '2', '3');
 
 // 튜플
-const tuple: [string, number] = ['1', 1];
-tuple[2] = 'hello'; // 🚨
-
-tuple.push('hello'); // 이건 못막아줌 타입스크립트의 한계점
+const tuple_3: [string, number] = ['1', 1];
+tuple_3[2] = 'hello'; // 에러
+tuple_3.push('hello'); // 에러X 타입스크립트의 사소한 단점
