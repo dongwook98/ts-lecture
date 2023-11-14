c0124036d71c819ee5684b68c636a168568a8f71
dongwook98 <ehddnr8563@gmail.com>
2023-11-14 20:55:37 +0900

    11/14 복습

--- 04.ts
+++ 04.ts
@@ -1,7 +1,7 @@
 // enum
 // js 변환시 사라진다.
 // 값들을 하나로 묶으고 싶을 때 사용
-const enum EDirection {
+const enum EDirection_4 {
   Up, // 0
   Down, // 1
   Left, // 2
@@ -10,19 +10,19 @@ const enum EDirection {
 
 // 값 지정도 가능
 // 문자열도 가능
-const enum EDirection {
+const enum EDirection2_4 {
   Up2 = '1',
   Down2 = 3,
   Left2 = 4,
   Right2 = 5,
 }
 
-const a_4 = EDirection.Up;
-const c_4 = EDirection.Left;
+const a_4 = EDirection_4.Up;
+const c_4 = EDirection_4.Left;
 
 // enum 대신 객체로 묶을 수 있다.
 // js 변환 시 안사라진다. 이 방법을 추천!
-const ODirection = {
+const ODirection_4 = {
   Up: 0,
   Down: 1,
   Left: 2,
@@ -32,8 +32,8 @@ const ODirection = {
 // 남겨야할지 안남겨야할지 모르겠다면 남겨라.
 
 // as const 지우면 타입추론을 제대로 못함
-// number라고 널널하게 하고 있음
-const ODirection2 = {
+// number라고 널널하게 하고 있다.
+const ODirection2_4 = {
   Up: 0,
   Down: 1,
   Left: 2,
@@ -41,7 +41,7 @@ const ODirection2 = {
 };
 
 // as const 사용 시 readonly도 붙어서 수정못하게함! 엄격하게 타이핑해줌.
-const ODirection3 = {
+const ODirection3_4 = {
   Up: 0,
   Down: 1,
   Left: 2,
@@ -50,19 +50,18 @@ const ODirection3 = {
 
 // enum 쓰는 코드
 // enum은 직접 타입으로 사용 쓸 수 있다.
-function walk(dir: EDirection) {}
+function walk_4(dir: EDirection_4) {}
 
-walk(EDirection.Left);
+walk_4(EDirection_4.Left);
 
 const obj_4 = { a: '123', b: 'hello', c: 'world' } as const; // 자바스크립트 값
-// 자바스크립트 값은 타입으로 사용할 수 없다. 그래서 typeof 키워드 붙여줌
+// 자바스크립트 값은 타입으로 사용할 수 없다. 그래서 앞에 typeof 키워드 붙여줌
 // 여기서 keyof를 사용해 객체의 키 값들을 타입으로 사용하는것!
-type Key = keyof typeof obj_4;
+type Key_4 = keyof typeof obj_4;
+type Value_4 = (typeof obj_4)[keyof typeof obj_4]; // value 들만 가져오는것!
 
-type Value = (typeof obj_4)[keyof typeof obj_4]; // value 들만 가져오는것!
+// enum 안쓰는 코드 (일반 객체를 타입으로 사용)
+type Direction_4 = (typeof ODirection3_4)[keyof typeof ODirection3_4];
+function run_4(dir: Direction_4) {}
 
-// enum 안쓰는 코드
-type Direction = (typeof ODirection)[keyof typeof ODirection];
-function run(dir: Direction) {}
-
-run(ODirection.Right);
+run_4(ODirection3_4.Right);
